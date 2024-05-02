c03e0fb5ebc432f94f0671df2cf3b9edf5a1669f
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-02 20:20:17 -0300

    chore: Add font-family to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
+        fontFamily: {
+            sans: ["Roboto"],
+        },
         extend: {},
     },
     plugins: [],
