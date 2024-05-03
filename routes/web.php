d0374c687ac04b0929eb858eb032606f6307f2b8
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-03 13:32:33 -0300

    feat: Update project routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+use App\Http\Controllers\EventController; //! Não entendi porque esse App tem que ser maiusculo, talvez seja porque no provider esta assim
+
+Route::get('/', [EventController::class, 'index']);
+//web.php file
+Route::prefix('/events')->group(__DIR__ . '/events.php');
+
+Route::get('/teste', function () {
+    $nome = 'caio';
+    $nome = 1;
+
+    return view('teste', ['name' => $nome]);
+});
+Route::get('/teste-params/{id?}', function ($id = 1) {
+
+    return view('teste-params', ['parametro' => $id]);
+});
+Route::get('/teste-search', function () {
+
+    $search = request('search');
+
+    $teste = request('teste');
+
+    return view('teste-search', ['search' => $search, 'teste' => $teste]);
+});
+
+
+Route::get('/blade', function () {
     return view('welcome');
 });
+
+
+Route::get('/returnJson', [EventController::class, 'returnJson']);
