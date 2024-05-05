2bc7145370ad78550d05b3e30be416393fbf7d0b
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-05 01:43:58 -0300

    test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,10 +9,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    {{-- <link href="/css/styles.css" rel="stylesheet" />
-    <script src="/js/script.js"></script> --}}
+    <link href="/css/styles.css" rel="stylesheet" />
+    <script src="/js/script.js"></script>
     <script src="{{ asset('js/script.js') }}" defer></script>
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
@@ -26,7 +27,7 @@
 
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="collapse navbar-collapse" id="navbar">
+            <div class=" navbar-collapse" id="navbar">
                 <a href="/" class="navbar-brand">
                     <img src="/img/hdcevents_logo.svg" alt="Events Logo">
                     Events</a>
